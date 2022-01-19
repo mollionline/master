c03e4cb941aabb4b240d9413eb14897d9bcb742a
moldir <@auezova_moldir>
2022-01-19 21:58:41 +0600

    #2 Создала модели для приложения таск трекер

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Status(models.Model):
+    status = models.CharField(max_length=50, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return '{}'.format(self.status)
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=50, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return '{}'.format(self.type)
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(max_length=1000, null=True, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+    def __str__(self):
+        return '{}. {}'.format(self.pk, self.summary)
