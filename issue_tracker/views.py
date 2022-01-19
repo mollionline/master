225861b91ff0777890855b0a8e863f25be18e4cc
moldir <@auezova_moldir>
2022-01-19 22:54:30 +0600

    Добавила вью для отображения главной страницы

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, RedirectView
+from issue_tracker.models import Task, Type, Status
+
 
 # Create your views here.
+class TaskListView(TemplateView):
+    template_name = 'list_task.html'
+    extra_context = {'title': 'Список задач'}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
