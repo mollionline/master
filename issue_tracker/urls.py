554f501a95c00fdaeea097832d99656f83887cc0
moldir <@auezova_moldir>
2022-01-20 11:17:22 +0600

    Добавила возможность просмотра одной задачи и изменила соответствующие файлы

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from issue_tracker.views import (TaskListView)
-
+from issue_tracker.views import (TaskListView,
+                                 DetailTaskView)
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='list_task')
+    path('', TaskListView.as_view(), name='list_task'),
+    path('tasks/<int:pk>/', DetailTaskView.as_view(), name='detail_task')
 ]
--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, RedirectView
 from issue_tracker.models import Task, Type, Status
 
@@ -12,3 +12,11 @@ class TaskListView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.all()
         return context
+
+
+class DetailTaskView(TaskListView):
+    template_name = 'detail_task.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['task'] = get_object_or_404(Task, pk=self.kwargs.get('pk'))
+        return super().get_context_data(**kwargs)
